539eefc680d8dbd938a9fe561ab26e3b9b8a553d
Giovanni Longo <giovanni.longo10@icloud.com>
2026-02-05 15:55:01 +0100

    Start Project

---
+++ src/main/java/GIOVANNILONGO/BE_GARA/BeGaraApplication.java
@@ -0,0 +1,13 @@
+package GIOVANNILONGO.BE_GARA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeGaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeGaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GIOVANNILONGO/BE_GARA/BeGaraApplicationTests.java
@@ -0,0 +1,13 @@
+package GIOVANNILONGO.BE_GARA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeGaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
