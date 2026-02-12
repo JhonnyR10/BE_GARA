6c79e47a1942ddd84359c9c90f44a56a58410b26
Giovanni Longo <giovanni.longo10@icloud.com>
2026-02-12 13:55:41 +0100

    allow fe origin

--- src/main/java/GIOVANNILONGO/BE_GARA/config/WebConfig.java
+++ src/main/java/GIOVANNILONGO/BE_GARA/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://fe-gara.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
